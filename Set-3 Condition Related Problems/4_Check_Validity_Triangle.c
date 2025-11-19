23578d8514f970ab292f3c9ccbceba8e47b895cd
Sharif2023 <sharifislam0505@gmail.com>
2025-11-19 21:03:43 +0600

    print eng dig

---
+++ Set-3 Condition Related Problems/4_Check_Validity_Triangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a1, a2, a3;
+    printf("Enter three angles: ");
+    scanf("%d%d%d", &a1, &a2, &a3);
+    if (a1 + a2 + a3 == 180)
+        printf("Yes\n");
+    else
+        printf("No\n");
+    return 0;
+}
